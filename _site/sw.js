fbd1e47f8a4ec7245b4fdb4823b3d108495927a9
Park Sung Jun <junstin119@gmail.com>
2023-11-14 00:50:06 +0900

    블로그 생성

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1699890049';
+const cacheName = 'chirpy-1699890571';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
