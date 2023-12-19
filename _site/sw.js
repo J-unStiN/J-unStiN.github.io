ddf20d6a786c9e25fa37563839ffe5ed1d77efe6
Park Sung Jun <junstin119@gmail.com>
2023-12-19 20:26:50 +0900

    블로그 글 전체 업데이트

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701173294';
+const cacheName = 'chirpy-1702985078';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
